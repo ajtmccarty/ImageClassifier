6a6cc16ac2a25ee5fedc2868b58a48b4651ae409
ajtmccarty <aaronjohntmccarty@gmail.com>
2019-06-21 07:50:51 -0700

    add generate_dataloaders method to ImageTrainer

--- train.py
+++ train.py
@@ -1,9 +1,21 @@
 from pathlib import Path
-from typing import List
+from typing import Dict, List
 
 from torch.nn import Module as NNModule
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import DatasetFolder, ImageFolder
+
 
 from cli_parser import create_training_parser
+
+
+class ImageTrainerError(Exception):
+    pass
+
+
+class ImageTrainerInitErro(ImageTrainerError):
+    pass
 
 
 class ImageTrainer:
@@ -25,6 +37,53 @@ class ImageTrainer:
         self.hidden_units: List[int] = hidden_units
         self.epochs: int = epochs
         self.gpu: bool = gpu
+
+        self.dataloaders: Dict[str, DataLoader] = {}
+
+    def generate_dataloaders(self, batch_size: int = 32):
+        """Initialize `dataloaders` instance attr"""
+        image_dirs: Dict[str, Path] = {
+            "train": Path(self.data_dir, 'train').absolute(),
+            "validation": Path(self.data_dir, 'valid').absolute(),
+            "test":  Path(self.data_dir, 'test').absolute()
+        }
+        # validate image dirs
+        for name, p in image_dirs.items():
+            if not (p.exists() and p.is_dir()):
+                raise ImageTrainerError(f"Path {p} for {name} image directory is not a valid directory")
+
+        the_transforms: Dict[str, transforms.Compose] = {
+            "train": transforms.Compose([
+                transforms.RandomResizedCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomRotation(45),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+            # validation is same as test for now
+            "validation": transforms.Compose([
+                transforms.Resize(255),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+            "test": transforms.Compose([
+                transforms.Resize(255),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+        }
+
+        image_datasets: Dict[str, DatasetFolder] = {
+            name: ImageFolder(str(image_dirs[name]), transform=the_transforms[name])
+            for name in image_dirs.keys()
+        }
+
+        self.dataloaders = {
+            name: DataLoader(image_datasets[name], batch_size=batch_size, shuffle=True)
+            for name in ["train", "validation", "test"]
+        }
 
 
 if __name__ == "__main__":
