28bebdf7531612cf97194c0f26dd473d66b45dda
ajtmccarty <aaronjohntmccarty@gmail.com>
2019-06-21 07:34:15 -0700

    add types to ImageTrainer initialization

--- train.py
+++ train.py
@@ -1,17 +1,31 @@
 from pathlib import Path
+from typing import List
+
+from torch.nn import Module as NNModule
 
 from cli_parser import create_training_parser
 
 
 class ImageTrainer:
-    def __init__(self, data_dir, save_dir, arch, learning_rate, hidden_units, epochs, gpu):
+    def __init__(
+            self,
+            data_dir: Path,
+            save_dir: Path,
+            arch: NNModule,
+            learning_rate: float,
+            hidden_units: List[int],
+            epochs: int,
+            gpu: bool
+    ):
+        # initial instance vars
         self.data_dir: Path = data_dir
-        self.save_dir = save_dir
-        self.arch = arch
-        self.learning_rate = learning_rate
-        self.hidden_units = hidden_units
-        self.epochs = epochs
-    
+        self.save_dir: Path = save_dir
+        self.arch: NNModule = arch
+        self.learning_rate: float = learning_rate
+        self.hidden_units: List[int] = hidden_units
+        self.epochs: int = epochs
+        self.gpu: bool = gpu
+
 
 if __name__ == "__main__":
     parser = create_training_parser()
