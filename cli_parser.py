4786405776b66d7880e4c9d04960148c3b560098
ajtmccarty <aaronjohntmccarty@gmail.com>
2019-06-22 12:53:29 -0700

    reformat cli_parser with 'black' code formatter

--- cli_parser.py
+++ cli_parser.py
@@ -15,13 +15,17 @@ class ArgTypes:
         try:
             return [int(x) for x in in_string.split(",")]
         except ValueError as exc:
-            raise ArgumentTypeError(f"{in_string} must be a comma-separated list of integers") from exc
+            raise ArgumentTypeError(
+                f"{in_string} must be a comma-separated list of integers"
+            ) from exc
 
     @staticmethod
     def torchvision_model_name(in_string: str) -> str:
         """Verify that in_string is the name of a supported model in torchvision.models"""
         if not re.search("^dense|alex|res|vgg", in_string):
-            raise ArgumentTypeError(f"This tool only supports DenseNet, ResNet, VGG, or AlexNet models")
+            raise ArgumentTypeError(
+                f"This tool only supports DenseNet, ResNet, VGG, or AlexNet models"
+            )
         if hasattr(torchvision.models, in_string):
             return in_string
         raise ArgumentTypeError(f"{in_string} is not a torchvision model")
@@ -43,20 +47,20 @@ class ArgTypes:
 def create_training_parser():
     parser = ArgumentParser(
         prog="train",
-        description="Train a neural net classifier for a pretrained model from PyTorch's torchvision module"
+        description="Train a neural net classifier for a pretrained model from PyTorch's torchvision module",
     )
     parser.add_argument(
         "data_dir",
         type=ArgTypes.valid_dir,
         help="Directory of the structure specified by PyTorch's ImageFolder class(https://pytorch.org"
-        "/docs/0.4.0/torchvision/datasets.html?highlight=imagefolder#imagefolder"
+        "/docs/0.4.0/torchvision/datasets.html?highlight=imagefolder#imagefolder",
     )
     parser.add_argument(
         "-s",
         "--save-dir",
         type=ArgTypes.abs_path,
         default="checkpoints",
-        help="Directory to save checkpoints of trained neural nets"
+        help="Directory to save checkpoints of trained neural nets",
     )
     parser.add_argument(
         "-a",
@@ -64,34 +68,35 @@ def create_training_parser():
         type=ArgTypes.torchvision_model_name,
         default="vgg16",
         help="Name of pretrained neural net to use. Must be one of the models in PyTorch's torchvision.models("
-             "https://pytorch.org/docs/0.4.0/torchvision/models.html) and must be one of the AlexNet, DenseNet, "
-             "ResNet, or VGG models"
+        "https://pytorch.org/docs/0.4.0/torchvision/models.html) and must be one of the AlexNet, DenseNet, "
+        "ResNet, or VGG models",
     )
     parser.add_argument(
         "-lr",
         "--learning-rate",
         type=float,
         default=0.003,
-        help="Learning rate for the model being trained"
+        help="Learning rate for the model being trained",
     )
     parser.add_argument(
         "-hu",
         "--hidden-units",
         type=ArgTypes.int_list,
         default="500,400",
-        help="Comma-separated list of ints representing the size of each hidden layer. A single layer (i.e. 400) is allowed."
+        help="Comma-separated list of ints representing the size of each hidden layer. A single layer "
+        "(i.e. 400) is allowed.",
     )
     parser.add_argument(
         "-e",
         "--epochs",
         type=int,
         default=10,
-        help="Number of epochs to train the model for"
+        help="Number of epochs to train the model for",
     )
     parser.add_argument(
         "-g",
         "--gpu",
         action="store_true",
-        help="Whether to use a GPU. Will only work if PyTorch has access to a GPU"
+        help="Whether to use a GPU. Will only work if PyTorch has access to a GPU",
     )
     return parser
